def86442a2ce4f73ad664b073fe6f9bd2186e5fd
sahan713 <sahanakalanka818@gmail.com>
2021-08-06 23:34:09 +0530

    index file

---
+++ index.php
@@ -0,0 +1,291 @@
+<?php include 'INC_HEAD.php';?>
+    <title>Sahan Gems-Home Page</title>
+    <style>
+        #zse1{
+            width: 100%;
+            height: 500px;
+            background-color: rgb(198, 239, 252);
+        }
+        #zd5{
+            width: 100%;
+            height: 500px;
+            display: flex;
+            justify-content: space-between;
+        }
+        .zd6{
+            width: 50%;
+            height: 500px;
+        }
+        #zd7{
+            /* padding: 159px 0; */
+            margin: 138px 0;
+        }
+        #zse2{
+            width: 100%;
+            height: 250px;
+            /* background-color: aqua; */
+        }
+        .zd8{
+            margin: 15px 0;
+        }
+        #zh11{
+            font-size: 35px;
+            font-weight:bold;
+        }
+        #zp1{
+            font-size: 18px;
+            margin: 13px 0;
+        }
+        #zd9{
+            margin: 90px 74px;
+        }
+
+        .avatar {
+            vertical-align: middle;
+            width: 180px;
+            height:180px;
+            border-radius: 50%;
+        }
+        #zd10{
+            width: 100%;
+            height: 250px;
+            display: flex;
+            justify-content: space-between;
+            /* padding: 59px 56px; */
+            margin: 35px 50px;
+        }
+        .zd11{
+            width: 25%;
+            height: 250px;
+        }
+        .zd12{
+            margin: 21px 26px;
+        }
+        .za2{
+            text-decoration: none;
+            color: #000;
+            font-weight: bold;
+        }
+        #zse3{
+            width: 100%;
+            height: 500px;
+            background-color: rgb(198, 239, 252);
+        }
+        .container{
+            padding: 73px 178px;
+        }
+        #h21{
+            font-size: 37px;
+            text-align: center;
+            padding: 10px 10px;
+            font-weight: bold;
+        }
+        .zp2{
+            padding: 10px;
+        }
+
+        /*Testimonials*/
+
+        /* Slideshow container */
+        .slideshow-container {
+        position: relative;
+        /* background: #f1f1f1f1; */
+        }
+
+        /* Slides */
+        .mySlides {
+        display: none;
+        /* padding: 80px; */
+        text-align: center;
+        }
+
+        /* Next & previous buttons */
+        .prev, .next {
+        cursor: pointer;
+        position: absolute;
+        top: 50%;
+        width: auto;
+        margin-top: -30px;
+        padding: 16px;
+        color: #888;
+        font-weight: bold;
+        font-size: 20px;
+        border-radius: 0 3px 3px 0;
+        user-select: none;
+        }
+
+        /* Position the "next button" to the right */
+        .next {
+        position: absolute;
+        right: 0;
+        border-radius: 3px 0 0 3px;
+        }
+
+        /* On hover, add a black background color with a little bit see-through */
+        .prev:hover, .next:hover {
+        background-color: rgba(0,0,0,0.8);
+        color: white;
+        }
+
+        /* The dot/bullet/indicator container */
+        .dot-container {
+            text-align: center;
+            padding: 20px;
+            /* background: #ddd; */
+        }
+
+        /* The dots/bullets/indicators */
+        .dot {
+        cursor: pointer;
+        height: 15px;
+        width: 15px;
+        margin: 0 2px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+        transition: background-color 0.6s ease;
+        }
+
+        /* Add a background color to the active dot/circle */
+        .active, .dot:hover {
+        background-color: #717171;
+        }
+
+        /* Add an italic font style to all quotes */
+        q {font-style: italic;}
+
+        /* Add a blue color to the author */
+        .author {color: cornflowerblue;}
+
+        #zse4{
+            width: 100%;
+            height: 370px;
+        }
+        #zh21{
+            text-align: center; 
+            margin: 35px 21px;
+            font-size: 34px;
+        }
+        .avatar2{
+            padding: 14px 0;
+            vertical-align: middle;
+            width: 180px;
+            height:180px;
+            border-radius: 50%;
+        }
+
+    </style>
+
+<?php include 'INC_NAVI.php';?>
+
+    <section id="zse1">
+        <div class="w">
+            <div id="zd5">
+                <div class="zd6">
+                    <div id="zd9">
+                        <lottie-player src="https://assets1.lottiefiles.com/datafiles/JpbqfIf5VoVu0T5/data.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+                    </div>
+                    
+                </div>
+    
+                <div class="zd6" id="zd7">
+                    <h1 id="zh11">Hey There! You Can Buy <br>Ceylon Gemstones Here</h1>
+                    <p id="zp1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aut maiores esse sit repudiandae amet illum. Beatae distinctio magnam, quibusdam est doloremque ullam amet, perferendis quae itaque laborum vero autem.</p>
+                    <div class="zd8">
+                        <button class="bt1"><a href="" class="a3">Shop Now</a></button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+    </section>
+
+    <section id="zse2">
+        <div class="w" id="zd13">
+            <div id="zd10">
+                <div class="zd11">
+                    <a href=""><img src="images/blue.jpg" alt="Avatar" class="avatar"></a>
+                    <div class="zd12">
+                        <a href="" class="za2">Blue Sapphire</a>
+                    </div>
+                </div>
+                <div class="zd11">
+                    <a href=""><img src="images/ruby.jpg" alt="Avatar" class="avatar"></a>
+                    <div class="zd12">
+                        <a href="" class="za2">Ceylon Ruby</a>
+                    </div>
+                </div>
+                <div class="zd11">
+                    <a href=""><img src="images/yellow.jpg" alt="Avatar" class="avatar"></a>
+                    <div class="zd12">
+                        <a href="" class="za2">Yellow Sapphire</a>
+                    </div>
+                </div>
+                <div class="zd11">
+                    <a href=""><img src="images/aquamarine.jpg" alt="Avatar" class="avatar"></a>
+                    <div class="zd12">
+                        <a href="" class="za2">Aquamarine</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section id="zse3">
+        <div class="w">
+            <div class="container">
+                <h1 id="h21">
+                    Who We Are?
+                </h1>
+                <p class="zp2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eaque adipisci magnam tempora provident sed iusto? Nam nesciunt voluptatibus, laudantium harum itaque deserunt corrupti. Fugit veniam alias natus sit ad.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus non praesentium vel itaque maiores ut, consequatur est saepe quas incidunt maxime suscipit doloremque labore quaerat similique, iure neque? Repellat, veritati Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique at, rerum vitae ipsam sequi sunt itaque ratione sed natus, enim, eos doloremque! Magnam consequatur sed illum fugit mollitia velit totam.
+                </p>
+                <p class="zp2">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore eaque adipisci magnam tempora provident sed iusto? Nam nesciunt voluptatibus, laudantium harum itaque deserunt corrupti. Fugit veniam alias natus sit ad.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus non praesentium vel itaque maiores ut, consequatur est saepe quas incidunt maxime suscipit doloremque labore quaerat similique, iure neque? Repellat, veritati Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique at, rerum vitae ipsam sequi sunt itaque ratione sed natus, enim, eos doloremque! Magnam consequatur sed illum fugit mollitia velit totam.
+                </p>
+            </div>
+        </div>
+    </section>
+   <section id="zse4">
+       <div class="w">
+        <div class="slideshow-container">
+            <h2 id="zh21">Testimonials</h2>
+    
+            <div class="mySlides">
+                <div>
+                    <img src="images/tes1.jpg" alt="testimonail-person" class="avatar2">
+                </div>
+              <q>I love you the more in that I believe you had liked me for my own sake and for nothing else</q>
+              <p class="author">- John Keats</p>
+            </div>
+            
+            <div class="mySlides">
+                <div>
+                    <img src="images/tes2.jpg" alt="testimonail-person" class="avatar2">
+                </div>
+              <q>But man is not made for defeat. A man can be destroyed but not defeated.</q>
+              <p class="author">- Ernest Hemingway</p>
+            </div>
+            
+            <div class="mySlides">
+                <div>
+                    <img src="images/tes3.jpg" alt="testimonail-person" class="avatar2">
+                </div>
+              <q>I have not failed. I've just found 10,000 ways that won't work.</q>
+              <p class="author">- Thomas A. Edison</p>
+            </div>
+            
+            <a class="prev" onclick="plusSlides(-1)">❮</a>
+            <a class="next" onclick="plusSlides(1)">❯</a>
+            
+            </div>
+            
+            <div class="dot-container">
+              <span class="dot" onclick="currentSlide(1)"></span> 
+              <span class="dot" onclick="currentSlide(2)"></span> 
+              <span class="dot" onclick="currentSlide(3)"></span> 
+            </div>
+       </div>
+   </section>
+   <?php include 'INC_FOOT.php';?>
